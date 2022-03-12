14376ad7199085b31cd9f5fceaf25e1bc53f79f5
Mateo <mateog91@gmail.com>
2022-03-12 11:44:52 -0500

    UPD: correctly implemented transactions endpoint for corresponding user_id, MISSING: add date range to the endpoint and filter movements by it

--- Back_End/flask-server/app/routes/transaction.py
+++ Back_End/flask-server/app/routes/transaction.py
@@ -2,19 +2,27 @@
 """ object that handles all default RestFul API actions for transactions """
 from fastapi import APIRouter
 from models import storage
+from models.category import Category
 from models.transaction import Transaction
 
 transaction = APIRouter()
 
-# @transaction.get('/user/{user-id}/transactions/')
-@transaction.get('/transaction')
-def get_transactions():
-    lst = []
-    for instance in storage.session.query(Transaction).order_by(Transaction.date.desc()):
-        lst.append(instance.__dict__.copy())
+
+@transaction.get('/user/{user_id}/transaction')
+def get_transactions(user_id: int):
+    # create an end point for get categories
+    # get categories from query filtered by the user_id
+    categories = storage.session.query(
+        Category).filter_by(user_id=user_id).all()
+    # create a dictionary with category id : category name
+    categories_ids = {
+        category.category_id: category.name for category in categories}
+
+    # list of transactions(dictionary representation) of corresponding user_id (those that are in the category_id dict)
+    transactions = [transaction for transaction in storage.session.query(
+        Transaction).order_by(Transaction.date.desc())
+        if transaction.category_id in categories_ids]
 
     storage.session.commit()
     storage.session.close()
-    return lst
-    # return storage.session.query(Transaction).order_by(Transaction.date.desc())
-    # return "hello world transactions"
+    return transactions
