8bcac83c5053216141d300e55d23c092dc306028
Mateo <mateog91@gmail.com>
2022-03-12 14:11:14 -0500

    UPD: implemented date filtering, dates in query parameters are optional with default value as None. Pending create endpoint for category requests and clean up code

--- Back_End/flask-server/app/routes/transaction.py
+++ Back_End/flask-server/app/routes/transaction.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """ object that handles all default RestFul API actions for transactions """
 from fastapi import APIRouter
+from typing import Optional
 from models import storage
 from models.category import Category
 from models.transaction import Transaction
-
+from datetime import date
 transaction = APIRouter()
 
 
 @transaction.get('/user/{user_id}/transaction')
-def get_transactions(user_id: int):
+def get_transactions(
+    user_id: int,
+    initial_date: Optional[date] = None,
+    final_date: Optional[date] = None
+    ):
     # create an end point for get categories
     # get categories from query filtered by the user_id
     categories = storage.session.query(
-        Category).filter_by(user_id=user_id).all()
+        Category).filter(Category.user_id == user_id).all()
     # create a dictionary with category id : category name
     categories_ids = {
         category.category_id: category.name for category in categories}
 
     # list of transactions(dictionary representation) of corresponding user_id (those that are in the category_id dict)
-    transactions = [transaction for transaction in storage.session.query(
-        Transaction).order_by(Transaction.date.desc())
+    transactions = [transaction for transaction in storage.session.query(Transaction).
+    filter(Transaction.date.between(initial_date, final_date)).
+    order_by(Transaction.date.desc())
         if transaction.category_id in categories_ids]
 
     storage.session.commit()
