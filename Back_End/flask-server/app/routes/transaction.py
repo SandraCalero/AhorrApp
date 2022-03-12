ba6075750f1f863ed229d58fb8c126abd8b66393
Mateo <mateog91@gmail.com>
2022-03-12 16:48:55 -0500

    UPD: transaction.py working for url /all-transactions/i_date=d1&f_date=d2

--- Back_End/flask-server/app/routes/transaction.py
+++ Back_End/flask-server/app/routes/transaction.py
@@ -5,6 +5,7 @@ from typing import Optional
 from models import storage
 from models.category import Category
 from models.transaction import Transaction
+from models.user import User
 from datetime import date
 transaction = APIRouter()
 
@@ -12,8 +13,8 @@ transaction = APIRouter()
 @transaction.get('/user/{user_id}/transaction')
 def get_transactions(
     user_id: int,
-    initial_date: Optional[date] = None,
-    final_date: Optional[date] = None
+    i_date: Optional[date] = "1900-01-01",
+    f_date: Optional[date] = "2099-12-31"
     ):
     # create an end point for get categories
     # get categories from query filtered by the user_id
@@ -23,12 +24,19 @@ def get_transactions(
     categories_ids = {
         category.category_id: category.name for category in categories}
 
+    # ***********START TEST ************
+    # get_categories = storage.session.query(
+    #     User).join(Category).first()
+    # print(type(get_categories.categories))
+
+
+    # ***********END TEST ************
+
     # list of transactions(dictionary representation) of corresponding user_id (those that are in the category_id dict)
     transactions = [transaction for transaction in storage.session.query(Transaction).
-    filter(Transaction.date.between(initial_date, final_date)).
+    filter(Transaction.date.between(i_date, f_date)).
     order_by(Transaction.date.desc())
         if transaction.category_id in categories_ids]
-
     storage.session.commit()
     storage.session.close()
     return transactions
