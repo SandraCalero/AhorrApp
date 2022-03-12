be53e830dab81fef403279ac93658d7b1ed1e1dc
Mateo <mateog91@gmail.com>
2022-03-12 12:57:41 -0500

    UPD: changed date column to Date data type

--- Back_End/flask-server/models/transaction.py
+++ Back_End/flask-server/models/transaction.py
@@ -2,8 +2,8 @@
 """contains the class definition of a State
     and an instance Base = declarative_base():
 """
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, DECIMAL
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, DECIMAL, Date
+from datetime import date
 from models.base_model import Base
 
 # Base = declarative_base()
@@ -15,7 +15,7 @@ class Transaction(Base):
     __tablename__ = 'transactions'
     transaction_id = Column(Integer, primary_key=True, autoincrement=True)
     description = Column(String(60), nullable=False)
-    date = Column(DateTime, default=datetime.now(), nullable=False)
+    date = Column(Date, default=date.today(), nullable=False)
     value = Column(DECIMAL(15, 2), nullable=False, default=0.0)
     category_id = Column(Integer, ForeignKey(
         'categories.category_id'), nullable=False)
