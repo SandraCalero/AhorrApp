d9de922c3a7a6e12e32028761a5bdeea6b031cc6
JuanSebastianGB <3393@holbertonschool.com>
2022-03-13 15:15:25 +0000

    UPD: Implementing base_model and DBstorage functions to handle the controller layer. Pending to check pydantic validations

--- Back_End/flask-server/models/user.py
+++ Back_End/flask-server/models/user.py
@@ -5,16 +5,15 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
+from models.base_model import Base, BaseModel
 
 # Base = declarative_base()
 
 
-class User(Base):
+class User(BaseModel, Base):
     """ Class for State inherited from Base
     """
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(45), nullable=False)
     last_name = Column(String(45), nullable=False)
     email = Column(String(45), nullable=False)
