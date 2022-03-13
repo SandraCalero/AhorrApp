48a1b567d83e3548d7f1aa35d02527ac86f1337e
JuanSebastianGB <3393@holbertonschool.com>
2022-03-13 02:35:03 +0000

    UPD: Adding data validation, and styling docs

--- Back_End/flask-server/schemas/transaction_type_schema.py
+++ Back_End/flask-server/schemas/transaction_type_schema.py
@@ -2,5 +2,10 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class Transaction_type_schema(BaseModel):
+class Transaction_type_schema_in(BaseModel):
     type: str
+
+
+class Transaction_type_schema_out(BaseModel):
+    type: str
+    transaction_type_id: int
