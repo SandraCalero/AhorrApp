9a62b399ed2fbfaa1f07ab4ee72318ee05966ce5
Mateo <mateog91@gmail.com>
2022-03-14 17:16:07 -0500

    ADD: category rout and schema files

---
+++ Back_End/flask-server/app/routes/category.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" object that handles all default RestFul API actions for transaction_types """
+from fastapi import APIRouter, Response, status, HTTPException
+from fastapi.responses import JSONResponse
+from pprint import pprint
+from models import storage
+from models.user import User
+from schemas.category_schema import CategorySchema
+from typing import List
+from fastapi.encoders import jsonable_encoder
+
+category = APIRouter()
+
+
+@category.get('/category', tags = ['categories'], status_code=200, response_model=List[CategorySchema])
+def get_categories():
+    return "hello world"
+
+@category.get('/user/{user_id}/categories', tags = ['categories'], status_code=200, response_model=List[CategorySchema])
+def get_categories(user_id: int):
+    user = storage.get('User', user_id)
+    print(user)
+    if user is None:
+        HTTPException(status_code=400, detail="User not found")
+
+    user_categories = user.categories
+
+    return [category for category in user_categories]
