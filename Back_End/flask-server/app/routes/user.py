13ada5ad01050ded86d5b331823300f7509cc861
Mateo <mateog91@gmail.com>
2022-03-14 20:36:46 -0500

    UPD: advances in user CRUD endpoints, NOT READY

--- Back_End/flask-server/app/routes/user.py
+++ Back_End/flask-server/app/routes/user.py
@@ -5,22 +5,37 @@ from fastapi.responses import JSONResponse
 from pprint import pprint
 from models import storage
 from models.user import User
-from schemas.user_schema import UserSchema
+from schemas.user_schema import UserSchema, UserCreate
 from typing import List, Dict
 from fastapi.encoders import jsonable_encoder
 
 user = APIRouter()
 
 
-@user.get(
-    '/user/{id}', 
-    tags = ['users'], 
-    status_code=200, 
-    response_model=UserSchema
-    )
-def get_user(id: int):
+@user.get('/user/{id}', tags=['users'], status_code=200, response_model=UserSchema)
+def get_user_by_id(id: int):
     user = storage.get(User, id)
-    print(id)
     if not user:
         raise HTTPException(status_code=400, detail="User not found")
     return user
+
+@user.get('/user-email/{email}', tags=['users'], status_code=200)
+def get_user_by_email(email: str):
+    if not email:
+        raise HTTPException(status_code=400, detail="No email input")
+    users = storage.all(User)
+    print(users)
+    if not users:
+        raise HTTPException(status_code=400, detail="User not found")
+    for user in users.values():
+        if email == user.email:
+            return user
+
+    raise HTTPException(status_code=400, detail=f"User with email {email} not found")
+
+# @user.post('/user', tags=['users'], status_code=201, response_model=UserSchema)
+# def create_user(user: UserCreate):
+#     db_user = get_user_by_email(user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail=" Email already registered")
+#     new_user = 
--- Back_End/flask-server/schemas/user_schema.py
+++ Back_End/flask-server/schemas/user_schema.py
@@ -2,12 +2,14 @@ from typing import Optional
 from pydantic import BaseModel
 from schemas.category_schema import CategorySchema
 
-
-class UserSchema(BaseModel):
-
-    first_name: str
-    last_name: str
+class UserBase(BaseModel):
+    first_name = str
+    last_name = str
     email: str
+
+class UserCreate(UserBase):
+    password: str
+class UserSchema(UserBase):
     categories: list[CategorySchema] = []
 
     class Config:
