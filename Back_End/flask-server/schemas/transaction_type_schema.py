0a1f2d9f6f682e4fafa892a629eec37785879091
JuanSebastianGB <3393@holbertonschool.com>
2022-03-15 05:04:00 +0000

    UPD:FULL CRUD OF categories

--- Back_End/flask-server/schemas/transaction_type_schema.py
+++ Back_End/flask-server/schemas/transaction_type_schema.py
@@ -1,14 +1,16 @@
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Transaction_type_schema_in(BaseModel):
     type: str
 
 
-class Transaction_type_schema_out(BaseModel):
-    type: str
-    transaction_type_id: int
+class Transaction_type_schema_out(Transaction_type_schema_in):
+    id: int
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_mode = True
--- Back_End/flask-server/schemas/user_schema.py
+++ Back_End/flask-server/schemas/user_schema.py
@@ -1,16 +1,20 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
 from schemas.category_schema import CategorySchema
+
 
 class UserBase(BaseModel):
     first_name = str
     last_name = str
     email: str
 
+
 class UserCreate(UserBase):
     password: str
+
+
 class UserSchema(UserBase):
-    categories: list[CategorySchema] = []
+    categories: Optional[List[CategorySchema]] = []
 
     class Config:
         orm_mode = True
