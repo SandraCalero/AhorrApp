0e2ad25878d65aa6079960dc7a104493a3d541d8
JuanSebastianGB <3393@holbertonschool.com>
2022-03-16 03:37:07 +0000

    UPD: Updating to get a full vertion of the transaction schema

--- Back_End/flask-server/schemas/transaction_schema.py
+++ Back_End/flask-server/schemas/transaction_schema.py
@@ -1,13 +1,31 @@
 from typing import Optional
 from pydantic import BaseModel
-from datetime import date
+from datetime import date, datetime
 
 
-class TransactionSchema(BaseModel):
+class TransactionBase(BaseModel):
     description: str
     date: date
     value: float
+    category_id: int
 
-    class Config:
-        orm_mode = True
 
+class ItemCreate(BaseModel):
+    pass
+
+
+class TransactionUpdate(BaseModel):
+    description: Optional[str] = None
+    date: Optional[date] = None
+    value: Optional[float] = None
+    category_id: Optional[int] = None
+
+
+class TransactionSchema(BaseModel):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class Config:
+    orm_mode = True
