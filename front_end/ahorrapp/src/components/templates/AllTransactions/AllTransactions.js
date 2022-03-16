bbd34e30807b35af9b6cadbc9984d12a4b800c74
Juanse1595 <3309@holberstonschool.com>
2022-03-16 18:45:23 -0500

    UPD added glass style to ConfirmationModal.css, added state for Confirmation Modal in useConfirmationModal so it can change between shown and not shown

--- front_end/ahorrapp/src/components/templates/AllTransactions/AllTransactions.js
+++ front_end/ahorrapp/src/components/templates/AllTransactions/AllTransactions.js
@@ -3,26 +3,28 @@ import { Title } from '../../atoms/Title/Title';
 import { DivButtons } from '../../molecules/DivButtons/DivButtons';
 import { NavBar } from '../../molecules/NavBar/NavBar';
 import { TransactionCard } from '../../molecules/TransactionCard/TransactionCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faArrowCircleUp,
-  faArrowCircleDown,
-} from '@fortawesome/free-solid-svg-icons';
 import { Footer } from '../../organisms/Footer/Footer';
 import './AllTransactions.css';
+import { ConfirmationModal } from '../../molecules/ConfirmationModal/ConfirmationModal';
+import { useAllTransactions } from './useAllTransactions';
 
 function AllTransactions() {
-  const incomeIcon = <FontAwesomeIcon icon={faArrowCircleUp} />;
-  const expenseIcon = <FontAwesomeIcon icon={faArrowCircleDown} />;
+  const { incomeIcon, expenseIcon, isConfirmationOpen, openConfiramtionModal } =
+    useAllTransactions();
   return (
     <div className="body">
+      <ConfirmationModal isConfirmationOpen={isConfirmationOpen} />
       <NavBar />
       <section className="container">
         <Title text="Transactions" />
         <DivButtons type="transaction" />
         <article className="transactionsBox">
           <div className="history">
-            <TransactionCard variant="IncomeCard" icon={incomeIcon} />
+            <TransactionCard
+              variant="IncomeCard"
+              icon={incomeIcon}
+              openConfiramtionModal={openConfiramtionModal}
+            />
             <TransactionCard variant="ExpenseCard" icon={expenseIcon} />
             <TransactionCard variant="IncomeCard" icon={incomeIcon} />
             <TransactionCard variant="ExpenseCard" icon={expenseIcon} />
