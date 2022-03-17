3e8da55891e2a7c76efa79dd49e19288e742eaf7
Mateo <mateog91@gmail.com>
2022-03-17 13:47:55 -0500

    UPD: changed transaction_schema date type to date_type to avoid an: date is not None error

--- Back_End/flask-server/schemas/transaction_schema.py
+++ Back_End/flask-server/schemas/transaction_schema.py
@@ -1,11 +1,11 @@
 from typing import Optional
 from pydantic import BaseModel
-from datetime import date, datetime
+from datetime import date as date_type, datetime
 
 
 class TransactionBase(BaseModel):
     description: str
-    date: date
+    date: date_type
     value: float
     category_id: int
 
@@ -14,9 +14,9 @@ class TransactionCreate(TransactionBase):
     pass
 
 
-class TransactionUpdate(TransactionCreate):
+class TransactionUpdate(TransactionBase):
     description: Optional[str] = None
-    date: Optional[date] = None
+    date: Optional[date_type] = None
     value: Optional[float] = None
     category_id: Optional[int] = None
 
