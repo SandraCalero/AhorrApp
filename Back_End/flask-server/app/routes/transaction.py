bb72ca6c664613d1b5c4c368a83c50fbf324d2e2
JuanSebastianGB <3393@holbertonschool.com>
2022-03-19 16:07:37 +0000

    UPD: Implementing required endpoint to get a custom dictionary with the transactions related data by user and checking pycodestyle

--- Back_End/flask-server/app/routes/transaction.py
+++ Back_End/flask-server/app/routes/transaction.py
@@ -4,13 +4,17 @@ from fastapi import APIRouter, Response, status, HTTPException
 from fastapi.responses import JSONResponse
 from models import storage
 from datetime import date
-from schemas.transaction_schema import TransactionCreate, TransactionSchema, TransactionUpdate
+from schemas.transaction_schema import TransactionCreate,\
+    TransactionSchema, TransactionUpdate, TransactionCustom
 from schemas.category_schema import CategorySchema
-from app.routes.category import get_all_categories, get_one_category, get_categories_by_user
+from app.routes.category import get_one_category
 from app.routes.user import get_user_by_id
 from models.transaction import Transaction
+from models.category import Category
+from models.transaction_type import TransactionType
 from typing import List, Optional
 import json
+from pprint import pprint
 
 transaction = APIRouter()
 
@@ -30,10 +34,12 @@ def create_transaction(transaction: TransactionCreate):
     return new_transaction
 
 
-@transaction.get('/transaction/{id}',
-                 tags=['transactions'],
-                 status_code=200,
-                 response_model=TransactionSchema)
+@transaction.get(
+    '/transaction/{id}',
+    tags=['transactions'],
+    status_code=200,
+    response_model=TransactionSchema
+)
 def get_transaction(id: int):
     """Get transactions by transaction id"""
     transaction = storage.get(Transaction, id)
@@ -42,10 +48,88 @@ def get_transaction(id: int):
     return transaction
 
 
-@transaction.get('/user/{user_id}/all-transactions',
-                 tags=['transactions'],
-                 status_code=201,
-                 response_model=List[TransactionSchema])
+def get_data_by_date(i_date, f_date, user_id):
+    """Function that valdiate the incoming dates to handle the flow"""
+    if type(i_date) is str or type(f_date) is str:
+        results = storage.session.query(
+            Transaction,
+            Category,
+            TransactionType
+        )\
+            .select_from(Transaction)\
+            .join(Category)\
+            .join(TransactionType)\
+            .filter(Category.user_id == user_id)\
+            .all()
+    else:
+        if i_date >= f_date:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="f_date cannot be before or same as i_date"
+            )
+        results = storage.session.query(
+            Transaction,
+            Category,
+            TransactionType
+        )\
+            .select_from(Transaction)\
+            .join(Category)\
+            .join(TransactionType)\
+            .filter(Category.user_id == user_id)\
+            .filter(Transaction.date.between(i_date, f_date))\
+            .all()
+    return results
+
+
+@transaction.get(
+    '/user/{user_id}/transactions',
+    tags=['transactions'],
+    status_code=201,
+    response_model=TransactionCustom
+)
+def custom_get_all_transactions_by_user(
+    user_id: int,
+    i_date: Optional[date] = "1900-01-01",
+    f_date: Optional[date] = "2099-12-31"
+):
+    """Getting all transactions of an user with a custome build"""
+    get_user_by_id(user_id)
+    results = get_data_by_date(i_date, f_date, user_id)
+    dictionary = {
+        'expenses': {'categories': {}, 'totalExpenses': 0},
+        'incomes': {'categories': {}, 'totalIncomes': 0},
+        'budget': {'categories': {}}
+    }
+    for transaction, category, transactiontype in results:
+        if transactiontype.id == 1:
+            dictionary['expenses']['totalExpenses'] =\
+                dictionary['expenses']['totalExpenses'] + transaction.value
+            if category.name not in dictionary['expenses']['categories']:
+                dictionary['expenses']['categories'].update(
+                    {category.name: transaction.value})
+            else:
+                dictionary['expenses']['categories'][category.name] =\
+                    dictionary['expenses']['categories'][category.name] + \
+                    transaction.value
+        if transactiontype.id == 2:
+            dictionary['incomes']['totalIncomes'] =\
+                dictionary['incomes']['totalIncomes'] + transaction.value
+            if category.name not in dictionary['incomes']['categories']:
+                dictionary['incomes']['categories'].update(
+                    {category.name: transaction.value})
+            else:
+                dictionary['incomes']['categories'][category.name] =\
+                    dictionary['incomes']['categories'][category.name] + \
+                    transaction.value
+    storage.session.close()
+    pprint(dictionary)
+    return dictionary
+
+
+@ transaction.get('/user/{user_id}/all-transactions',
+                  tags=['transactions'],
+                  status_code=201,
+                  response_model=List[TransactionSchema])
 def get_all_transactions_by_user(
     user_id: int,
     i_date: Optional[date] = "1900-01-01",
@@ -62,8 +146,9 @@ def get_all_transactions_by_user(
 
     categories_ids = {category.id: category.name for category in categories}
 
-    all_transactions = storage.session.query(Transaction).filter(
-        Transaction.date.between(i_date, f_date)).order_by(Transaction.date.desc())
+    all_transactions = storage.session.query(Transaction)\
+        .filter(Transaction.date.between(i_date, f_date))\
+        .order_by(Transaction.date.desc())
 
     transactions = [
         transaction for transaction in all_transactions
@@ -74,10 +159,10 @@ def get_all_transactions_by_user(
     return transactions
 
 
-@transaction.put('/transaction/{id}',
-                 tags=['transactions'],
-                 status_code=200,
-                 response_model=TransactionSchema)
+@ transaction.put('/transaction/{id}',
+                  tags=['transactions'],
+                  status_code=200,
+                  response_model=TransactionSchema)
 def update_trasaction(
         id: int,
         transaction_data: TransactionUpdate):
@@ -98,9 +183,9 @@ def update_trasaction(
     return transaction
 
 
-@transaction.delete('/transaction/{id}',
-                    tags=['transactions'],
-                    status_code=status.HTTP_200_OK)
+@ transaction.delete('/transaction/{id}',
+                     tags=['transactions'],
+                     status_code=status.HTTP_200_OK)
 def delete_transaction(id: int):
     """Delete a transaction by its id"""
     transaction = get_transaction(id)
--- Back_End/flask-server/schemas/transaction_schema.py
+++ Back_End/flask-server/schemas/transaction_schema.py
@@ -1,9 +1,16 @@
-from typing import Optional
+#!/usr/bin/python3
+
+"""Creates all clases to handle like schema validation into
+transactions
+"""
+
+from typing import Optional, Dict
 from pydantic import BaseModel
 from datetime import date as date_type, datetime
 
 
 class TransactionBase(BaseModel):
+    """Validation transaction Base used to require transaction data"""
     description: str
     date: date_type
     value: float
@@ -11,17 +18,27 @@ class TransactionBase(BaseModel):
 
 
 class TransactionCreate(TransactionBase):
+    """Used to handle optional validations"""
     pass
 
 
 class TransactionUpdate(TransactionBase):
+    """Used to validate data in put operation"""
     description: Optional[str] = None
     date: Optional[date_type] = None
     value: Optional[float] = None
     category_id: Optional[int] = None
 
 
+class TransactionCustom(BaseModel):
+    """Used to validate data in custom get operation"""
+    expenses: dict
+    incomes: dict
+    budget: dict
+
+
 class TransactionSchema(TransactionBase):
+    """Used to show all the transaction data"""
     id: int
     created_at: datetime
     updated_at: datetime
